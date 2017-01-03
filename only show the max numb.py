9af0cb799a4b9ba0de316682c9b8ecf6942eb91c
AceChampion <810345199@qq.com>
2017-01-03 08:58:26 +0800

    Update only show the max numb.py

--- only show the max numb.py
+++ only show the max numb.py
@@ -1,16 +1,15 @@
 #counti type in numbs and print max
-#ver1
+#ver2
+temp = input('please inpuut\n\ta num(q to quit):')
+a = [int(temp)]
 while True:
-    num = input('please inpuut\n\ta num(q to quit):')
-    if num.lower == 'q':
+    temp = input('please inpuut\n\ta num(q to quit):')
+    if temp.lower == 'q':
         break
-    num = int(num)
-    MaxSoFar = num
-    print (MaxSoFar)
-    num = input('please inpuut\n\ta num(q to quit):')
-    if num.lower == 'q':
-        break
-    num = int(num)
-    if MaxSoFar < num:
-            MaxSoFar = int(num)
-    print (MaxSoFar)
+    num = int(temp)
+    a = a.append(num)
+    Max = a[0]
+    for each in a:
+        if each > Max:
+            Max = each
+    print(Max)
